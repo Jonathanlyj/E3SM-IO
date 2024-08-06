9a2b4f36df5a0e04ba052cf9194413adf9767174
Jonathanlyj <youjia@northwestern.edu>
2024-08-06 15:51:53 -0700

    add job script for perl

---
+++ job.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH -A m844
+#SBATCH -t 00:2:00
+#SBATCH -N 1
+#SBATCH -C cpu
+#SBATCH --qos=debug
+
+#SBATCH -o qout.256.%j # std::out 输出到这个文件 
+#SBATCH -e qout.256.%j 
+
+#SBATCH --mail-type=end,fail
+#SBATCH --mail-user=youjia@northwestern.edu
+
+if test "x$SLURM_NTASKS_PER_NODE" = x ; then #number of cores per node
+   SLURM_NTASKS_PER_NODE=16 # 256 maximum
+fi
+
+NUM_NODES=$SLURM_JOB_NUM_NODES
+
+NP=$((NUM_NODES * SLURM_NTASKS_PER_NODE))
+
+ulimit -c unlimited
+PRELOAD=/pscratch/sd/y/yll6162/soft/romio-install/lib/libromio.so
+EXE=/global/homes/y/yll6162/E3SM-IO/src/e3sm_io 
+SB_EXE=/tmp/${USER}_test #tmp all users shared
+sbcast -v ${EXE} ${SB_EXE} #executable copy to this position
+
+LD_PRELOAD=${PRELOAD} srun -n $NP -c $((256/$SLURM_NTASKS_PER_NODE)) --cpu_bind=cores ${SB_EXE} -o /pscratch/sd/y/yll6162/FS_1M_4/can_F_out.nc /pscratch/sd/y/yll6162/FS_1M_4/map_f_case_16p.nc
+# 8 process per node => 256/8=32
